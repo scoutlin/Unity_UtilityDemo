7fd1b0fc409df288eda7267af6224bfc1aa7417e
DESKTOP-MGHFEMN\ScottLin <scoutlinn@gmail.com>
2017-11-27 17:43:12 +0800

    FullScreen Test

---
+++ Assets/Script/WebGL_FullScreenTest/Controller/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WebGL_FullScreenTest
+{
+    public class MainController : MonoBehaviour
+    {
+        public Text Text_ClickInfoFullScreeTip;
+        public Text Text_MainScene;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnText_ClickInfoFullScreeTipClick()
+        {
+            Text_ClickInfoFullScreeTip.gameObject.SetActive(false);
+            Text_MainScene.gameObject.SetActive(true);
+
+            Screen.fullScreen = true;
+        }
+
+        public void OnText_MainSceneClick()
+        {
+            bool isFullScreen = Screen.fullScreen;
+            Text_MainScene.text = "Is FullScreen: " + isFullScreen;
+        }
+    }
+}
